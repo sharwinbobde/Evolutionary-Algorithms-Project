233117e08fc04b43c01542b8ebeaa4286ad0c4f0
sharwinbobde <bsharwin@gmail.com>
2020-06-10 11:57:16 +0200

    pyswarm testbench done

--- particle_swarm/particle.py
+++ particle_swarm/particle.py
@@ -3,14 +3,30 @@ import numpy as np
 
 class Particle:
 
-    def __init__(self, graph_G, bkv:int):
+    def __init__(self, graph_manager):
         super().__init__()
-        self.G = graph_G
+        self.gm = graph_manager
+        self.bkv = graph_manager.bkv
 
-        self.edges = self.G.edges(data=False)
-        self.array_ = np.zeros((self(len(self.edges))))
+        # dimentions are number of nodes
+        # 0 1 indicates sets A and B
+        self.nodes = self.gm.G.nodes(data=False)
+        self.array = np.zeros(self.gm.G.number_of_nodes())
 
-    def compute_cost(self):
-        ''' cost function '''
-        
-        pass
+    def compute_cost(self, param):
+        ''' cost function 
+        aarry element being 0 indicates set_A and 1 indicates set_B
+        '''
+        set_A = []
+        set_B = []
+
+        for index, x in np.ndenumerate(param):
+            if x == 0:
+                set_A.append(index[0] + 1)
+            else:
+                set_B.append(index[0] + 1)
+        cost = self.gm.cut_cost(set_A, set_B)
+
+        # turn maximization to minimization
+        return self.bkv - cost
+    
--- particle_swarm/pyswarm_manager.py
+++ particle_swarm/pyswarm_manager.py
@@ -1,19 +1,44 @@
 import pyswarms as ps
 from pyswarms.utils.functions import single_obj as fx
+from particle_swarm.particle import Particle
+import numpy as np
 
 class PySwarmManager:
 
-    def __init__(self, max_iter = 1000):
+    def __init__(self, particles, graph_manager, options=None, max_iter = 1000):
         super().__init__()
         self.max_iter = max_iter
-    
-    def initialise_solver(self, particles=10, edges=10, options=None):
+        self.n_particles = particles
+        self.gm = graph_manager
+
+        self.optima_found = False
+
+        self.particles_array = [Particle(graph_manager) for i in range(particles)]
+
         if options == None:
             options = {'c1': 0.5, 'c2':0.3, 'w':0.9, 'k':3, 'p':2}
-        self.optimizer =ps.binary.BinaryPSO(n_particles=particles, dimensions=edges, options=options)
 
-    def make_function(self):
-        self.fx = fx.sphere
+        bounds = (0.0, self.gm.G.size(weight='weight')) # unused :( no bounds for binary po
+        # print(bounds)
+
+        # dimensions is same as no of nodes
+        self.optimizer =ps.binary.BinaryPSO(n_particles=particles, dimensions=graph_manager.G.number_of_nodes(), options=options)
+
+    def objective_function(self, params):
+        losses = []
+        for i in range(self.n_particles):
+            losses.append(self.particles_array[i].compute_cost(params[i]))
+        return np.array(losses)
 
     def optimise(self):
-        cost, pos = self.optimizer.optimize(self.fx, self.max_iter)
+
+        cost, pos = self.optimizer.optimize(self.objective_function, self.max_iter)
+
+        if cost == 0.0:
+            self.optima_found=0
+
+        self.elite_cost = cost
+        self.elite_param = pos
+
+    def get_elite(self):
+        return self.elite_param
